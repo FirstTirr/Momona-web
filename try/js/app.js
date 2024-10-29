d5bc3eb12684d53dc09337649ccade88250fba31
FirstTirr <fathiradzans@gmail.com>
2024-10-29 18:05:23 +0700

    momona file

---
+++ try/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener("alpine:init", () => {
+  Alpine.data("produk", () => ({
+    items: [
+      { id: 1, name: "baju satu", img: "baju1.jpg", price: 250000 },
+      { id: 2, name: "baju dua", img: "baju2.jpg", price: 280000 },
+      { id: 3, name: "baju tiga", img: "baju3.jpg", price: 200000 },
+      { id: 4, name: "baju empat", img: "baju4.jpg", price: 190000 },
+      { id: 5, name: "baju lima", img: "baju5.jpg", price: 210000 },
+    ],
+  }));
+});
